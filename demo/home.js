5e87d98f515b8d4d723375892b91bf5a59d9daac
Rasbin Rijal <rasbin.rijal@gmail.com>
2018-03-15 15:50:03 +0200

    Minor changes

--- demo/home.js
+++ demo/home.js
@@ -5,10 +5,10 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Hero from '../src/Hero';
 import heroImage from '../assets/dsconf-wide.jpg';
 
-import { Icon, Section, SectionHeader, SectionText } from '../lib/my-awesome-library'
+
+import { Icon, Hero, Section, SectionHeader, SectionText } from '../lib/my-awesome-library'
 
 const st = Section.SectionTypography
 
@@ -16,19 +16,15 @@ const Home = () => {
   return (
     <div>
         <Hero heroImage = {heroImage} />
-      <div>
-        <Icon name='star' color='#000' isInverted size='0.785rem' />
-        <Icon name='star' color='#888888' isInverted />
-        <Icon name='github' color='#1b8ceb'isInverted size='1.5rem' />
-        <Icon name='gift' color='#00d1b2' isInverted size='2.0rem' />
-        <Icon name='bug' color='#e91e63' isInverted size='3rem' />
-      </div>
-      <div>
         <Section>
-          <SectionHeader>Hello!</SectionHeader>
-          <SectionText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</SectionText>
+          <SectionHeader>Test</SectionHeader>
+          <SectionText>Text inside the section (with typography).</SectionText>
         </Section>
-      </div>
+        <Section isInverted>
+          <SectionHeader isInverted>Inverted Section</SectionHeader>
+          <SectionText isInverted>Text inside the section (with typography).</SectionText>
+        </Section>
+        
     </div>
   )
 }
